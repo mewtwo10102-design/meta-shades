9408fab67135701f14bc5de04d417e8ed93ce2bf
mewtwo10102-design <mewtwo10102@gmail.com>
2026-01-11 16:11:52 +0200

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Meta Shades ($SPECS) - See The Future',
+  description: 'Turn any meme into legendary shades! The smoothest memecoin on Solana.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
